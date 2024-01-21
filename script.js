4b96e0a825ca3a50f62331177a3f1894ad1514f2
Lutfino Athallah Pratama <finolut@gmail.com>
2024-01-21 11:00:09 +0700

    finishing dan mengubah beberapa di js dan html

--- script.js
+++ script.js
@@ -3,21 +3,42 @@ let song = document.getElementById("song");
 let ctrlIcon = document.getElementById("ctrlIcon");
 
 song.onloadedmetadata = function() {
-    progress.max = song.duration; // correcte d line
+    progress.max = song.duration;
     progress.value = song.currentTime;
 }
 
-function playPause() {
-    console.log('Play/Pause button clicked');
-        if(ctrlIcon.classList.contains("fa-pause")) {
-            song.pause();
-            ctrlIcon.classList.remove("fa-pause");
-            ctrlIcon.classList.add("fa-play");
-        } 
-        else {
-            song.play();
-            ctrlIcon.classList.add("fa-pause");
-            ctrlIcon.classList.remove("fa-play");
+window.addEventListener('DOMContentLoaded', function() {
+    function playPause() {
+        var audio = document.getElementById('song');
+        var playButton = document.getElementById('ctrlicon');
+        if (audio.paused) {
+            audio.play();
+            playButton.classList.remove('fa-play');
+            playButton.classList.add('fa-pause');
+        } else {
+            audio.pause();
+            playButton.classList.remove('fa-pause');
+            playButton.classList.add('fa-play');
         }
     }
-  
+
+    var playButton = document.querySelector('.controls div:nth-child(2)');
+    playButton.addEventListener('click', playPause);
+});
+
+
+song.ontimeupdate = function() {
+    progress.value = song.currentTime;
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    var playButton = document.querySelector('.controls div:nth-child(2)');
+    playButton.addEventListener('click', playPause);
+});
+
+progress.onchange = function(){
+    song.play();
+    song.currentTime = progress.value;
+    playButton.classList.remove('fa-pause');
+    playButton.classList.add('fa-play');
+}
